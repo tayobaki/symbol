1e407d40e317208e51d452d22719bdda498f84b2
tayobaki <109907707+tayobaki@users.noreply.github.com>
2024-01-19 09:58:28 +0100

    Update index.js

--- libs/index.js
+++ libs/index.js
@@ -1,14 +1,13 @@
 import { productData } from "@/constants/data";
 
+
+
 export const getProducts = async () => {
-  const res = await fetch("http://localhost:3000/api/products");
+  const res = productData;
 
-  if (!res.ok) {
-    throw new Error("Failed Fetch");
-  }
-
-  return res.json();
+  return res;
 };
+
 
 export const getSingleProduct = (slug) => {
   const product = productData.find((product) => product.slug === slug);
